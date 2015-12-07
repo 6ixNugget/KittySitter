92ebab56adb9b2d69cc2f060a31937344474b36f
Allen Yuze Zhang <allen.uc.uoft@icloud.com>
2015-12-06 21:17:52 -0500

    new post done

--- app/public/javascripts/newPost.js
+++ app/public/javascripts/newPost.js
@@ -1,24 +1,24 @@
-$('#np_submit').click(function(){
-    var formData = new FormData($('form')[0]);
-    $.ajax({
-        url: '/api/newPost',  //Server script to process data
-        type: 'POST',
-        xhr: function() {  // Custom XMLHttpRequest
-            var myXhr = $.ajaxSettings.xhr();
-            if(myXhr.upload){ // Check if upload property exists
-                myXhr.upload.addEventListener('progress',progressHandlingFunction, false); // For handling the progress of the upload
-            }
-            return myXhr;
-        },
-        //Ajax events
-        beforeSend: beforeSendHandler,
-        success: completeHandler,
-        error: errorHandler,
-        // Form data
-        data: formData,
-        //Options to tell jQuery not to process data or worry about content-type.
-        cache: false,
-        contentType: false,
-        processData: false
+$('#np_form').submit(function(event){
+    event.preventDefault();
+    photo = $('#np_image').val();
+    title = $('#np_title').val();
+    description = $('#np_description').val();
+    address = $('#np_address').val();
+    contact = $('#np_email').val();
+    startDate = $('#np_start_date').val();
+    endDate = $('#np_end_date').val();
+    var new_post = {
+        photo: photo,
+        title: title,
+        description: description,
+        address: address,
+        contact: contact,
+        startDate: new Date(startDate),
+        endDate: new Date(endDate)
+    };
+    $.post('/api/newPost',new_post,function(data){
+        if(data.status){
+            window.location.replace('/post/'+ data.post_id);
+        }
     });
 });
